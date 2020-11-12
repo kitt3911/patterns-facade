84c6b0b773ea31d89ecbfe8bbf98f084b657ca13
kitt3911 <ivanignatenko28@gmail.com>
2020-11-12 12:18:42 +0600

    init

---
+++ README.md
@@ -0,0 +1,14 @@
+### Facade pattern
+-----
+
+Facade = ComplaintRegistry
+
+----
+#### How to start?
+* 1.clone this repo
+* 2.write in your console : `yarn start`
+
+----
+Expected Result :
+
+![Illustration](./result.png)
---
+++ index.js
@@ -0,0 +1,47 @@
+class Complaints {
+    constructor() {
+        this.complaints = []
+    }
+
+    reply(complaint) { } //  abstract 
+
+    add(complaint) {
+        this.complaints.push(complaint)
+        return this.reply(complaint)
+    }
+}
+
+class ProductComplaints extends Complaints {
+    reply({ id, customer, details }) {
+        return `Product: ${id} : ${customer} (${details})`
+    }
+}
+
+
+class ServiceComplaints extends Complaints {
+    reply({ id, customer, details }) {
+        return `Service: ${id} : ${customer} (${details})`
+    }
+}
+
+
+class ComplaintRegistry {
+    register(customer, type, details) {
+        let complaint
+        const id = Date.now()
+        if (type === 'service') {
+            complaint = new ServiceComplaints()
+        }
+        else {
+            complaint = new ProductComplaints()
+        }
+
+        return complaint.add({ id, customer, details })
+    }
+}
+
+
+const registry = new ComplaintRegistry()
+
+console.log(registry.register('Ivan', 'service', 'dont allow'))
+console.log(registry.register('Alex', 'product', 'error'))
